77a72411baee108065d510757b8f5aa8cb56521e
Arpit Rajput <arpitrajput6424@gmail.com>
2024-05-11 12:57:15 +0530

    Bcrypt the password

--- src/main/java/org/example/config/SecurityAppConfig.java
+++ src/main/java/org/example/config/SecurityAppConfig.java
@@ -1,5 +1,6 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,23 +22,31 @@ import java.util.Arrays;
 @EnableWebSecurity(debug = true)
 public class SecurityAppConfig {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Bean
-    InMemoryUserDetailsManager setUpUser() {
-
-        ArrayList<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("admin"));
-        authorities.add( new SimpleGrantedAuthority("user"));
-        authorities.add(new SimpleGrantedAuthority("visitor"));
-        UserDetails user1 = new User("user1","password1", authorities);
-
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-        inMemoryUserDetailsManager.createUser(user1);
-
-        return inMemoryUserDetailsManager;
+    public InMemoryUserDetailsManager setUpUser(){
+        UserDetails user1 = User.withUsername("user1")
+                                .password( passwordEncoder.encode("password1") )
+                                .roles("admin","user","visitor")
+                                .build();
+        UserDetails user2 = User.withUsername("user2")
+                                .password(passwordEncoder.encode("password2") )
+                                .roles("user","visitor")
+                                .build();
+        return new  InMemoryUserDetailsManager(user1,user2);
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
+
+//    @Bean
+//    PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
+
 }

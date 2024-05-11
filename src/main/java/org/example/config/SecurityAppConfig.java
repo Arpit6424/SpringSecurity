8b64f2c5e07c612154b5083d6a66e49391b5ac85
Arpit Rajput <arpitrajput6424@gmail.com>
2024-05-11 14:42:12 +0530

    new styling for spring security

--- src/main/java/org/example/config/SecurityAppConfig.java
+++ src/main/java/org/example/config/SecurityAppConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -14,6 +16,8 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,6 +28,9 @@ public class SecurityAppConfig {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    HttpSecurity httpSecurity;
     @Bean
     public InMemoryUserDetailsManager setUpUser(){
         UserDetails user1 = User.withUsername("user1")
@@ -41,11 +48,42 @@ public class SecurityAppConfig {
     BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
-
 //    @Bean
 //    PasswordEncoder passwordEncoder(){
 //        return NoOpPasswordEncoder.getInstance();
+//    }
+
+
+    @Bean
+    SecurityFilterChain settingUpSecurity() throws Exception {
+
+        //old styling for security
+//        httpSecurity.authorizeHttpRequests().anyRequest().permitAll();
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/hi").authenticated();
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/bye").denyAll();
+//        httpSecurity.authorizeHttpRequests().requestMatchers("/hello").permitAll();
+
+//        httpSecurity.formLogin();
+//        httpSecurity.httpBasic();
+
+
+        httpSecurity.authorizeHttpRequests(customizer->{
+           customizer.requestMatchers("/hi").authenticated();
+           customizer.requestMatchers("/bye").hasRole("admin");
+           customizer.requestMatchers("/hello").permitAll();
+        });
+
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+
+        return httpSecurity.build();
+    }
+
+    //mvcHandlerMappingIntrospector
+//    @Bean
+//    HandlerMappingIntrospector handlerMappingIntrospector(){
+//        return new HandlerMappingIntrospector();
 //    }
 
 
